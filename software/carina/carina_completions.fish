3110a3e3b26ddaff2ffee5723401983899b93084
Arcade Wise <l3gacy.b3ta@gmail.com>
2022-01-10 16:01:04 -0500

    Add edit to carina

--- software/carina/carina_completions.fish
+++ software/carina/carina_completions.fish
@@ -5,3 +5,4 @@ complete -c carina -f
 complete -c carina -a create -r -d "Create a new file with a basic template"
 complete -c carina -a check -f -d "Check for errors"
 complete -c carina -a publish -f -d "Publish the content via git"
+complete -c carina -a edit -f -d "Edit an existing post"
